87a3c2018d0b7eb2fd4025a51bc4e6c43a443c8d
Amy  Tran <trantm.amy@gmail.com>
2021-07-20 14:53:43 -0700

    add routers

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,17 +11,21 @@ import { Subscription } from 'rxjs';
 export class HeaderComponent implements OnInit {
   title: string = 'Todo'; //pass title to header.html
   showAddTask: boolean = false;
-  subscription: Subscription;  //to watch the change on toggle button
+  subscription: Subscription; //to watch the change on toggle button
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService
       .onToggle()
-      .subscribe(value => (this.showAddTask = value));
+      .subscribe((value) => (this.showAddTask = value));
   }
 
   ngOnInit(): void {}
 
   toggleAdd() {
-   this.uiService.toggleAddTask();
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {  //*ngIf="hasRoute('/')"
+    return this.router.url === route;
   }
 }
